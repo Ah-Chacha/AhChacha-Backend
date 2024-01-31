9bd099d7aa9bc9b1c0a61d9763405dbc56bbbee4
이영선 <leedrkr323@konkuk.ac.kr>
2024-01-31 21:27:52 +0900

    [fix] application.yml 을 자동 복사하도록 설정 (workflow.yml)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,5 +28,5 @@ chmod +x $JAR_NAME
 
 echo ">> $JAR_NAME 실행 "
 nohup java -jar \
-        -Dspring.config.location=classpath:$REPOSITORY/zip/application.yml \
+        -Dspring.config.location=classpath:deploy/$PROJECT_NAME.zip \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
