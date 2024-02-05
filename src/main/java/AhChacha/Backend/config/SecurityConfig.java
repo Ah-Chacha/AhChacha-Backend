53d46612db7810579721b099dfc37a1bc992d92a
이영선 <leedrkr323@konkuk.ac.kr>
2024-02-05 19:14:52 +0900

    [feat] 리버스 프록시 profile API 생성

--- src/main/java/AhChacha/Backend/config/SecurityConfig.java
+++ src/main/java/AhChacha/Backend/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
                         // 아이콘, css, js 관련
                         // 기본 페이지, css, image, js 하위 폴더에 있는 자료들은 모두 접근 가능, h2-console에 접근 가능
                         .requestMatchers("/").permitAll()
-                        .requestMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
+                        .requestMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**", "/profile").permitAll()
                         .requestMatchers("/api/member/sign-up").permitAll() // 회원가입 접근 가능
                         .requestMatchers("/auth/token").permitAll()
                         .anyRequest().permitAll()) // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
