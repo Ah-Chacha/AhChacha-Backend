aa282cac3a9c181baf9e5c3109a79d8bd4c4a01f
이영선 <leedrkr323@konkuk.ac.kr>
2024-02-06 23:34:19 +0900

    [fix] pid 설정 수정

--- src/main/java/AhChacha/Backend/BackendApplication.java
+++ src/main/java/AhChacha/Backend/BackendApplication.java
@@ -9,7 +9,6 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 //		SpringApplication.run(BackendApplication.class, args);
-//     새로운 파일이 commit 되면, 이전 버전은 중단시킨 후 업데이트 된 jar를 실행하기 위해 pid command 값 설정 해주기
 		SpringApplication application = new SpringApplication(BackendApplication.class);
 		application.addListeners(new ApplicationPidFileWriter());
 		application.run(args);
