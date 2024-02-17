0ac5a168945d205a83758c5e9b5335ecf0c55950
이영선 <leedrkr323@konkuk.ac.kr>
2024-02-17 23:21:50 +0900

    [feat] swagger 적용

---
+++ src/main/java/AhChacha/Backend/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package AhChacha.Backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+@OpenAPIDefinition(
+        servers = @Server(url = "/", description = "Default Server URL"),
+        info = @Info(
+                title = "💡아차차 백엔드 API 명세",
+                description = "springdoc을 이용한 Swagger API 문서입니다.",
+                version = "1.0",
+                contact = @Contact(
+                        name = "springdoc 공식문서",
+                        url = "https://springdoc.org/"
+                )
+        )
+)
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("JTW", bearerAuth()));
+    }
+
+    private SecurityScheme bearerAuth() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+    }
+}
